3fad0c558e3d04ef51bc5ef49f63dd6317e1c6c1
derekn4 <derek.nguyen99@gmail.com>
2022-10-15 14:19:15 -0700

    loads directory of image url folders

--- pillImageDownload.py
+++ pillImageDownload.py
@@ -25,23 +25,39 @@ def download_images(images,url, folder_name): #, folder_name):
             print(f" {count} images have been downloaded out of {len(images)}")
 
 
-def main(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    links = []
-    for link in soup.find_all("a"):
-        #print(link)
-        if link.get("href").endswith(".JPG"):
-            #print(link.get("href"))
-            links.append(link.get("href"))
+def main(urls):
+    for url in urls:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        links = []
+        for link in soup.find_all("a"):
+            #print(link)
+            if link.get("href").endswith(".JPG"):
+                #print(link.get("href"))
+                links.append(link.get("href"))
 
-    if url.endswith('/index.html'):
-        url = url[:-11]
-    #download_images(links, url)
-    folder_create(links, url)
+        if url.endswith('/index.html'):
+            url = url[:-11]
+        #download_images(links, url)
+        folder_create(links, url)
 
-url = input("Enter site URL:")
-main(url)
-# import os 
-# import inspect 
-# print(os.path.dirname(inspect.getfile(inspect))+"/site-packages")
+#url = input("Enter site URL:")
+#main(url)
+
+url = "https://data.lhncbc.nlm.nih.gov/public/Pills/index.html"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+links = []
+for link in soup.find_all("a"):
+    if link.get("href").startswith("PillProjectDisc"):
+        links.append(link.get("href"))
+
+for l in range(len(links)):
+    if links[l].endswith('/index.html'):
+        links[l] = links[l][:-11]
+        links[l] = "https://data.lhncbc.nlm.nih.gov/public/Pills/" + links[l] + "/images/index.html"
+
+# for l in links:
+#     print(l)
+
+main(links[:5])
