cb9e8066f32f9c115ffc06e68484d164c3b9e4e3
derekn4 <derek.nguyen99@gmail.com>
2022-10-25 16:22:59 -0700

    Update pillImageDownload.py

--- pillImageDownload.py
+++ pillImageDownload.py
@@ -46,32 +46,35 @@ def download_images(images,url, folder_name): #, folder_name):
 
 
 def main(urls):
+    #iterates through list of urls
     for url in urls:
         r = requests.get(url)
         soup = BeautifulSoup(r.text, 'html.parser')
         links = []
         for link in soup.find_all("a"):
-            #print(link)
             if link.get("href").endswith(".JPG"):
-                #print(link.get("href"))
                 links.append(link.get("href"))
 
         if url.endswith('/index.html'):
             url = url[:-11]
-        #download_images(links, url)
         folder_create(links, url)
 
-#url = input("Enter site URL:")
-#main(url)
-
+#Main url
 url = "https://data.lhncbc.nlm.nih.gov/public/Pills/index.html"
+
+#request url
 r = requests.get(url)
+
+#parse with BeautifulSoup
 soup = BeautifulSoup(r.text, 'html.parser')
+
+#Search for hrefs --> link to inner folders
 links = []
 for link in soup.find_all("a"):
     if link.get("href").startswith("PillProjectDisc"):
         links.append(link.get("href"))
 
+#Test Code
 curr = [l.strip('/index.html') for l in links[5:10]]
 count = 0
 for c in curr:
@@ -79,16 +82,13 @@ for c in curr:
     count+=1
     if count==5:
         print()
+
+#Cleans links in folder
 for l in range(len(links)):
     if links[l].endswith('/index.html'):
         links[l] = links[l][:-11]
         links[l] = "https://data.lhncbc.nlm.nih.gov/public/Pills/" + links[l] + "/images/index.html"
 
-# for l in links:
-#     print(l)
-#folder_name = "images"
-#os.mkdir(folder_name)
-#print(links[:5])
-#print(links[2:5])
+
 print("Starting Image Downloading:", curr)
 main(links[5:10])
